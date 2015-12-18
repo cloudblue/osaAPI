e01abbc3592ff6c4c5c7b41b4ae84a35233c19c9
Anton Larkin <alarkin@odin.com>
2015-12-18 18:02:29 +0300

    Add jenkins.sh Add odintools

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from distutils.core import setup
+import os
+
+PACKAGE_VERSION = '0.1'
+
+
+def version():
+    import odintools
+
+    return odintools.version(PACKAGE_VERSION, os.environ.get('BUILD_NUMBER'))
 
 setup(
     name='paAPI',
-    version='0.0.1',
     author='oznu',
     author_email='dev@oz.nu',
     packages=['paAPI'],
@@ -10,4 +18,6 @@ setup(
     license='Apache License',
     description='A python client for the Parallels Operations (POA) and Business Automation (PBA-E) APIs.',
     long_description=open('README.rst').read(),
+    setup_requires=['odintools'],
+    odintools=True,
 )
