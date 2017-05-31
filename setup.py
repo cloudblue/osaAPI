2e0da7e01c19aedb1249ea756f89ad12714764f8
akhaerov <akhaerov@odin.com>
2017-05-31 16:21:26 +0300

    Use X.X.X version format, version only from setup.py file

--- setup.py
+++ setup.py
@@ -1,30 +1,16 @@
-import os
+#!/usr/bin/env python
 
 from setuptools import setup
-
-PACKAGE_VERSION = '0.3'
-
-
-def version():
-    def version_file(mode='r'):
-        return open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'version.txt'), mode)
-
-    if os.getenv('TRAVIS'):
-        with version_file('w') as verfile:
-            verfile.write('{0}.{1}'.format(PACKAGE_VERSION, os.getenv('TRAVIS_BUILD_NUMBER')))
-    with version_file() as verfile:
-        data = verfile.readlines()
-        return data[0].strip()
 
 
 setup(
     name='osaapi',
-    version=version(),
-    author='apsliteteam, oznu',
+    version='0.4.0',
+    author='apsconnect team, original by oznu',
     author_email='aps@odin.com',
     packages=['osaapi'],
     url='https://aps.odin.com',
     license='Apache License',
-    description='A python client for the Odin Service Automation (OSA) and billing APIs.',
+    description='A python binding for the Odin Service Automation (OSA) and billing APIs.',
     long_description=open('README.md').read(),
 )
